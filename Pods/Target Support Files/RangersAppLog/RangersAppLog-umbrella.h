6754f7c808455e50893ae991cdee179d0aa59956
pii ha <hapiisix6@gmail.com>
2024-06-26 14:00:20 +0800

    add

---
+++ Pods/Target Support Files/RangersAppLog/RangersAppLog-umbrella.h
@@ -0,0 +1,48 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "BDAutoTrack+Extension.h"
+#import "BDAutoTrack+Game.h"
+#import "BDAutoTrack+GameTrack.h"
+#import "BDAutoTrack+LaunchFrom.h"
+#import "BDAutoTrack+OhayooGameTrack.h"
+#import "BDAutoTrack+Profile.h"
+#import "BDAutoTrack+SharedInstance.h"
+#import "BDAutoTrack+Special.h"
+#import "BDAutoTrack.h"
+#import "BDAutoTrackAlinkRouting.h"
+#import "BDAutoTrackCacheRemover.h"
+#import "BDAutoTrackConfig+AppLog.h"
+#import "BDAutoTrackConfig+BDroneMonitor.h"
+#import "BDAutoTrackConfig.h"
+#import "BDAutoTrackEncryptionDelegate.h"
+#import "BDAutoTrackNotifications.h"
+#import "BDAutoTrackSchemeHandler.h"
+#import "BDCommonDefine.h"
+#import "BDCommonEnumDefine.h"
+#import "RangersAppLog.h"
+#import "RangersAppLogCore.h"
+#import "BDAutoTrackDevTools.h"
+#import "BDAutoTrackEncryptor.h"
+#import "BDAutoTrackExposure.h"
+#import "BDAutoTrackURLHostItemCN.h"
+#import "RangersAppLogPicker.h"
+#import "BDAutoTrack+UITracker.h"
+#import "BDKeyWindowTracker.h"
+#import "RangersAppLogUITrack.h"
+#import "UIBarButtonItem+TrackInfo.h"
+#import "UIView+TrackInfo.h"
+#import "UIViewController+TrackInfo.h"
+
+FOUNDATION_EXPORT double RangersAppLogVersionNumber;
+FOUNDATION_EXPORT const unsigned char RangersAppLogVersionString[];
+
--- range_c/VolcanoPlatform.swift
+++ range_c/VolcanoPlatform.swift
@@ -6,3 +6,107 @@
 //
 
 import Foundation
+import RangersAppLog
+
+enum RangeProfileKey: String {
+    /// 推送是否开启
+    case is_push_on
+    /// 用户code 尾号
+    case user_code_tail
+}
+
+extension String {
+    struct Volcano {
+#if DEBUG
+        static let appID = "10000001"
+        static let appName = "Sereal"
+#else
+        static let appID = "10000005"
+        static let appName = "Sereal_online"
+#endif
+        static let channel = "App Store"
+        static let host = "https://collect.9em.cn"
+    }
+}
+
+class VolcanoPlatform {
+    
+    class func start(_ launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) {
+        
+        let config = BDAutoTrackConfig(appID:.Volcano.appID, launchOptions: launchOptions)
+        config.channel = .Volcano.channel
+        config.appName = .Volcano.appName
+        config.serviceVendor = .private
+        
+        BDAutoTrack.setRequestHostBlock {(vendor: BDAutoTrackServiceVendor, requestURLType:BDAutoTrackRequestURLType) -> String? in
+            return .Volcano.host
+        }
+        BDAutoTrack.setCustomHeaderBlock {
+            return [:]
+        }
+        config.autoTrackEnabled = true
+        config.autoTrackEventType = .all
+        config.logNeedEncrypt = true
+        config.abEnable = true
+        config.enableH5Bridge = false
+        config.h5AutoTrackEnabled = false
+        /// 曝光
+        /*
+         config.exposureEnabled = true
+         config.exposureConfig.areaRatio(0.5)
+         config.exposureConfig.enableVisualDiagnosis(true)
+         
+         let opt = BDViewExposureData()
+         opt.eventName = "view_did_exposure"
+         opt.properties = [:]
+         opt.config = BDViewExposureConfig.default().areaRatio(0.5)
+         BDAutoTrack.shared().observeViewExposure(self, with: opt)
+         //BDAutoTrack.shared().disposeViewExposure(self)
+         */
+        
+#if DEBUG
+        config.devToolsEnabled = true
+        config.showDebugLog = true
+        
+        
+#else
+        config.devToolsEnabled = false
+        config.showDebugLog = false
+#endif
+        BDAutoTrack.sharedTrack(with: config)
+//        // 如果需要设置当前登陆态 since 6.13.0+
+//        UserStatusChangeBus.EventBus.register(type: .login, obj: self) { _ in
+//            BDAutoTrack.clearUserUniqueID()
+//            BDAutoTrack.setCurrentUserUniqueID(AppManager.default.user?.memberId)
+//            profileUserCodeTail()
+//            
+//        }
+//        BDAutoTrack.setCurrentUserUniqueID(AppManager.default.user?.memberId)
+//        profileUserCodeTail()
+        // 授权后
+        BDAutoTrack.shared().start()
+    }
+    
+    /// 设置用户属性
+    class func profile(key: RangeProfileKey, value: Any) {
+        let profileDict: [AnyHashable: Any] = [
+            key.rawValue : value
+        ]
+        BDAutoTrack.profileSet(profileDict)
+    }
+
+    class func stat(event: String, params: [String : Any?]? = nil) {
+        let dic: [String : Any?] = params ?? [:]
+        BDAutoTrack.eventV3(event, params: dic as [AnyHashable : Any])
+    }
+    
+    class func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        
+        for context in URLContexts {
+            let url = context.url
+            if BDAutoTrackSchemeHandler.shared().handle(url, appID: .Volcano.appID, scene: scene) {
+                
+            }
+        }
+    }
+}
